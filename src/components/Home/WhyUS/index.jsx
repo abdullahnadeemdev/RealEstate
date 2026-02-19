09320b6dd996ccd8969c6062b4ac38846336ab11
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-02-19 17:02:41 +0500

    why us page added responsivenesss remains

--- src/components/Home/WhyUS/index.jsx
+++ src/components/Home/WhyUS/index.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { homeInterior } from "../../../assets/images";
+import Button from "../../shared/Button";
+import { TickBox } from "../../../assets/icons";
 
 const WhyUs = () => {
-  return <div>WhyUs</div>;
+  return (
+    <div className="flex justify-between px-6 md:px-15 py-20 gap-6">
+      <div className="flex-1 relative mr-10 h-[74vh]">
+        <div className="absolute -z-10 h-[74vh] w-[99%] bg-secondary -right-5">
+          ghy
+        </div>
+        <img src={homeInterior} alt="" />
+      </div>
+      <div className="flex-1">
+        <p className="font-semibold text-secondary">-Why choose us?</p>
+        <p className="text-4xl font-semibold text-text-prim my-3">
+          Why Choose Mian & Sons Construction
+        </p>
+        <p className="text-greyLight mb-5">
+          ADCO Construction Limited delivers the highest quality workmanship
+          with a customer first approach. We offer reliable and affordable
+          services with clear communication that ensures every project is
+          completed on time, on budget and exceeds expectations.s
+        </p>
+
+        <span className="flex text-greyLight mb-3">
+          <TickBox />
+          <p>High quality workmanship</p>
+        </span>
+        <span className="flex text-greyLight mb-3">
+          <TickBox />
+          <p>Customer-focused approach</p>
+        </span>
+        <span className="flex text-greyLight mb-3">
+          <TickBox />
+          <p>Fair and transparent pricing</p>
+        </span>
+        <span className="flex text-greyLight mb-3">
+          <TickBox />
+          <p>Trusted local reputation</p>
+        </span>
+
+        <Button>Get Started</Button>
+      </div>
+    </div>
+  );
 };
 
 export default WhyUs;
--- src/pages/HomeP/index.jsx
+++ src/pages/HomeP/index.jsx
@@ -4,6 +4,7 @@ import FloatingDiv from "../../components/Home/FloatingDiv/FloatingDiv";
 import Values from "../../components/Home/Values";
 import Services from "../../components/Home/Services";
 import CTA from "../../components/Home/CTA";
+import WhyUs from "../../components/Home/WhyUS";
 
 const index = () => {
   return (
@@ -13,6 +14,7 @@ const index = () => {
       <Values />
       <Services />
       <CTA />
+      <WhyUs />
     </div>
   );
 };
